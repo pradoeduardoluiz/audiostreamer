e3376d1646ba63a22f692dbe7af2bd4db872c578
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2019-12-02 14:24:36 -0300

    Update MediaControllerFragment

--- app/src/main/java/br/com/pradoeduardoluiz/spotifyclone/ui/MediaControllerFragment.kt
+++ app/src/main/java/br/com/pradoeduardoluiz/spotifyclone/ui/MediaControllerFragment.kt
@@ -2,12 +2,15 @@ package br.com.pradoeduardoluiz.spotifyclone.ui
 
 
 import android.os.Bundle
+import android.support.v4.media.MediaMetadataCompat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import br.com.pradoeduardoluiz.spotifyclone.R
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_media_controller.*
 
 class MediaControllerFragment : Fragment() {
 
@@ -15,6 +18,29 @@ class MediaControllerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_media_controller, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_play_pause.setOnClickListener {
+
+        }
+
+    }
+
+    fun setMediaTitle(mediaItem: MediaMetadataCompat) {
+        text_media_song_title.text = mediaItem.description.title
+    }
+
+    fun setIsPlaying(isPlaying: Boolean) {
+        if (isPlaying) {
+            Glide.with(requireActivity()).load(R.drawable.ic_pause_circle_outline_white_24dp)
+                .into(button_play_pause)
+        } else {
+            Glide.with(requireActivity()).load(R.drawable.ic_play_circle_outline_white_24dp)
+                .into(button_play_pause)
+        }
+    }
 
 
 }
