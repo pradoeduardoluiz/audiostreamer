17e775662570f417201d95551fc7023c5610fc21
Luiz Eduardo do Prado <prado.eduardo.luiz@gmail.com>
2020-01-15 21:14:11 -0300

    Media Button Receivers and Pending Intents

--- app/src/main/java/br/com/pradoeduardoluiz/spotifyclone/notifications/MediaNotificationManager.kt
+++ app/src/main/java/br/com/pradoeduardoluiz/spotifyclone/notifications/MediaNotificationManager.kt
@@ -3,7 +3,9 @@ package br.com.pradoeduardoluiz.spotifyclone.notifications
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Build
 import android.support.v4.media.MediaDescriptionCompat
@@ -16,12 +18,46 @@ import androidx.core.content.ContextCompat
 import androidx.media.session.MediaButtonReceiver
 import br.com.pradoeduardoluiz.spotifyclone.R
 import br.com.pradoeduardoluiz.spotifyclone.services.MediaService
+import br.com.pradoeduardoluiz.spotifyclone.ui.MainActivity
+
 
 class MediaNotificationManager constructor(private val mediaService: MediaService) {
 
     private val notificationManager: NotificationManager =
         mediaService.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
+    private val playAction: NotificationCompat.Action = NotificationCompat.Action(
+        R.drawable.ic_play_arrow_white_24dp,
+        mediaService.getString(R.string.label_play),
+        MediaButtonReceiver.buildMediaButtonPendingIntent(
+            mediaService, PlaybackStateCompat.ACTION_PLAY
+        )
+    )
+    private var pauseAction: NotificationCompat.Action = NotificationCompat.Action(
+        R.drawable.ic_pause_circle_outline_white_24dp,
+        mediaService.getString(R.string.label_play),
+        MediaButtonReceiver.buildMediaButtonPendingIntent(
+            mediaService, PlaybackStateCompat.ACTION_PAUSE
+        )
+    )
+    private var nextAction: NotificationCompat.Action = NotificationCompat.Action(
+        R.drawable.ic_skip_next_white_24dp,
+        mediaService.getString(R.string.label_play),
+        MediaButtonReceiver.buildMediaButtonPendingIntent(
+            mediaService, PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+        )
+    )
+    private var previousAction: NotificationCompat.Action = NotificationCompat.Action(
+        R.drawable.ic_skip_previous_white_24dp,
+        mediaService.getString(R.string.label_play),
+        MediaButtonReceiver.buildMediaButtonPendingIntent(
+            mediaService, PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+        )
+    )
+
+    init {
+        notificationManager.cancelAll()
+    }
 
     public fun getNotificationManager() = notificationManager
 
@@ -55,6 +91,9 @@ class MediaNotificationManager constructor(private val mediaService: MediaServic
         description: MediaDescriptionCompat,
         bitmap: Bitmap
     ): Notification {
+
+        var isPlaying: Boolean = state.state == PlaybackStateCompat.STATE_PLAYING
+
         if (isAndroidOorHigher()) {
             createChannel()
         }
@@ -67,7 +106,7 @@ class MediaNotificationManager constructor(private val mediaService: MediaServic
         )
             .setColor(ContextCompat.getColor(mediaService, R.color.notification_bg))
             .setSmallIcon(R.drawable.ic_audiotrack_grey_24dp) // Pending intent that is fired when user clicks on notification.
-            .setContentIntent(null) // Title - Usually Song name.
+            .setContentIntent(createContentIntent()) // Title - Usually Song name.
             .setContentTitle(description.title) // Subtitle - Usually Artist name.
             .setContentText(description.subtitle)
             .setLargeIcon(bitmap) // When notification is deleted (when playback is paused and notification can be
@@ -79,7 +118,28 @@ class MediaNotificationManager constructor(private val mediaService: MediaServic
             ) // Show controls on lock screen even when user hides sensitive content.
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
 
+        if ((state.actions and PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS) != 0L) {
+            builder.addAction(previousAction)
+        }
+
+        builder.addAction(if (isPlaying) pauseAction else playAction)
+
+        if ((state.actions and PlaybackStateCompat.ACTION_SKIP_TO_NEXT) != 0L) {
+            builder.addAction(nextAction)
+        }
+
         return builder.build()
+    }
+
+    private fun createContentIntent(): PendingIntent {
+        val openUI = Intent(mediaService, MainActivity.javaClass)
+        openUI.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        return PendingIntent.getActivity(
+            mediaService,
+            501,
+            openUI,
+            PendingIntent.FLAG_CANCEL_CURRENT
+        )
     }
 
     companion object {
